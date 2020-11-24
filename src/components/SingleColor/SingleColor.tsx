8541e51e672e39a188043ef5b387f9b272efcfc9
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-11-24 23:06:18 +0100

    Added SingleColor component & functionality

--- src/components/SingleColor/SingleColor.tsx
+++ src/components/SingleColor/SingleColor.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import rgbToHex from '../../utils/rgbToHex';
 
-const SingleColor: React.FC = () => {
-  return <div>SingleColors</div>;
+interface SingleColorProps {
+  rgb: number[];
+  weight: number;
+  index: number;
+}
+
+const SingleColor: React.FC<SingleColorProps> = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+
+  const background = rgb.join(',');
+  // @ts-ignore: Unreachable code error
+  const hex = rgbToHex(...rgb);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color`}
+      style={{ backgroundColor: `rgb(${background})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+    >
+      <p>{weight}%</p>
+      <p>{hex}</p>
+      {alert && <p>copied to clipboard</p>}
+    </article>
+  );
 };
 
 export default SingleColor;
